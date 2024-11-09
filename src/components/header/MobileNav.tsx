c941f734632720f5a80af92c2d1b4bf04867f53f
André Davies <andredavies.dev@gmail.com>
2024-11-09 17:25:53 -0300

    feat: create mobile nav component

---
+++ src/components/header/MobileNav.tsx
@@ -0,0 +1,56 @@
+import { MouseEvent, useState } from "react";
+import { Box, IconButton, Menu, MenuItem, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+type TComponentProps = {
+  pages: string[];
+};
+
+export default function MobileNav({ pages }: TComponentProps) {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleOpenNavMenu}
+        color="inherit"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+        sx={{ display: { xs: "block", md: "none" } }}
+      >
+        {pages.map((page) => (
+          <MenuItem key={page} onClick={handleCloseNavMenu}>
+            <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+}
