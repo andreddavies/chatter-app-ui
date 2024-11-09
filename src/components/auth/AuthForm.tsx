ad82d8ab63e5d74cdae7dcfbd73a01e46ce2d9cb
André Davies <andredavies.dev@gmail.com>
2024-11-09 15:55:34 -0300

    Feat: Update authform to accept error prop and implement new interface

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import { Button, Stack, TextField } from "@mui/material";
 
-type TFormData = {
-  email: string;
-  password: string;
-};
+import { ICreateUserInput } from "../../services/user/create";
 
 type TComponentProps = {
+  error?: string;
   submitLabel: string;
-  onSubmit: (data: TFormData) => Promise<void>;
+  onSubmit: (data: ICreateUserInput) => Promise<void>;
 };
 
-const AuthForm = ({ onSubmit, submitLabel }: TComponentProps) => {
+const AuthForm = ({ error, onSubmit, submitLabel }: TComponentProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -22,6 +20,8 @@ const AuthForm = ({ onSubmit, submitLabel }: TComponentProps) => {
         type="email"
         label="Email"
         value={email}
+        error={!!error}
+        helperText={error}
         onChange={(event) => setEmail(event.target.value)}
       />
 
@@ -30,10 +30,15 @@ const AuthForm = ({ onSubmit, submitLabel }: TComponentProps) => {
         type="password"
         label="Password"
         value={password}
+        error={!!error}
+        helperText={error}
         onChange={(event) => setPassword(event.target.value)}
       />
 
-      <Button variant="contained" onClick={() => onSubmit({ email, password })}>
+      <Button
+        variant="contained"
+        onClick={() => onSubmit({ createUserInput: { email, password } })}
+      >
         {submitLabel}
       </Button>
     </Stack>
