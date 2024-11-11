d4627a408dd72816d0c95ffdff93e7353e4f092a
André Davies <andredavies.dev@gmail.com>
2024-11-11 19:10:40 -0300

    feat: update logout util to handle authentication state

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -1,7 +1,9 @@
 import router from "@components/router";
 import client from "@constants/apollo-client";
+import { authenticatedVar } from "@constants/authenticated";
 
 export const onLogout = () => {
+  authenticatedVar(false);
   router.navigate("/login");
   client.resetStore();
 };
