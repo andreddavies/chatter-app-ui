64b19f8130249d2cfbf060d22d41f1d01ae5fad2
André Davies <andredavies.dev@gmail.com>
2024-11-11 18:59:04 -0300

    feat: update apollo-client to use onLogout function

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -3,7 +3,7 @@ import { onError } from "@apollo/client/link/error";
 
 import { API_URL } from "./urls";
 import { excludedRoutes } from "./excluded-routes";
-import router from "@components/router";
+import { onLogout } from "@utils/logout";
 
 const logoutLink = onError((error) => {
   if (
@@ -12,8 +12,7 @@ const logoutLink = onError((error) => {
       401
   ) {
     if (!excludedRoutes.includes(window.location.pathname)) {
-      router.navigate("/login");
-      client.resetStore();
+      onLogout();
     }
   }
 });
