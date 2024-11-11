c590dea68a6b8b4c85f2ac200eeca55f3aa4a9b3
André Davies <andredavies.dev@gmail.com>
2024-11-11 20:04:43 -0300

    feat: update chat list header component

--- src/components/chat-list/ChatListHeader.tsx
+++ src/components/chat-list/ChatListHeader.tsx
@@ -1,11 +1,15 @@
 import { AppBar, IconButton, Toolbar } from "@mui/material";
 import AddCircle from "@mui/icons-material/AddCircle";
 
-export default function ChatListHeader() {
+type TComponentProps = {
+  handleAddChat: VoidFunction;
+};
+
+export default function ChatListHeader({ handleAddChat }: TComponentProps) {
   return (
     <AppBar position="static" color="transparent">
       <Toolbar>
-        <IconButton size="large" edge="start">
+        <IconButton size="large" edge="start" onClick={handleAddChat}>
           <AddCircle />
         </IconButton>
       </Toolbar>
